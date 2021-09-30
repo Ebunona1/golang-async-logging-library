0d24987c615a76b58122ba73e710ec4927a9c286
Ebunona1 <ebunona1@gmail.com>
2021-09-30 16:45:13 +0100

    handled panic situation

--- alog.go
+++ alog.go
@@ -29,14 +29,20 @@ func New(w io.Writer) *Alog {
 		w = os.Stdout
 	}
 	return &Alog{
-		dest: w,
+		dest:    w,
+		m:       &sync.Mutex{},
+		msgCh:   make(chan string),
+		errorCh: make(chan error),
 	}
 }
 
 // Start begins the message loop for the asynchronous logger. It should be initiated as a goroutine to prevent
 // the caller from being blocked.
 func (al Alog) Start() {
-
+	for {
+		msg := <-al.msgCh
+		go al.write(msg, nil)
+	}
 }
 
 func (al Alog) formatMessage(msg string) string {
@@ -47,21 +53,39 @@ func (al Alog) formatMessage(msg string) string {
 }
 
 func (al Alog) write(msg string, wg *sync.WaitGroup) {
+	// If panic occurs after .Write, will just to end of function.
+	// This will bypass the Unlock on ln 66, as this is defered, it is called when
+	// the function gets to the end, a check for if a panic occured is made and if so
+	// Unlock is called again.
+
+	defer func() {
+		if err := recover(); err != nil {
+			al.m.Unlock()
+		}
+	}()
+
+	al.m.Lock()
+	_, err := al.dest.Write([]byte(al.formatMessage(msg)))
+	al.m.Unlock()
+
+	if err != nil {
+		al.errorCh <- err
+	}
 }
 
 func (al Alog) shutdown() {
 }
 
 // MessageChannel returns a channel that accepts messages that should be written to the log.
-func (al Alog) MessageChannel() chan string {
-	return nil
+func (al Alog) MessageChannel() chan<- string {
+	return al.msgCh
 }
 
 // ErrorChannel returns a channel that will be populated when an error is raised during a write operation.
 // This channel should always be monitored in some way to prevent deadlock goroutines from being generated
 // when errors occur.
-func (al Alog) ErrorChannel() chan error {
-	return nil
+func (al Alog) ErrorChannel() <-chan error {
+	return al.errorCh
 }
 
 // Stop shuts down the logger. It will wait for all pending messages to be written and then return.
